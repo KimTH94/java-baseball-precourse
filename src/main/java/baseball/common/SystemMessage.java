633819a509e816ae17f906e35b2bc8b66ed3c1bc
KimTH94 <kcc940602@gmail.com>
2022-04-15 02:05:30 +0900

    fear(SystemMessage) create SystemMessage

---
+++ src/main/java/baseball/common/SystemMessage.java
@@ -0,0 +1,10 @@
+package baseball.common;
+
+public class SystemMessage {
+
+    public static final String START_PROGRAM = "숫자 야구 프로그램을 시작합니다.";
+    public static final String END_PROGRAM = "숫자 야구 프로그램을 종료합니다.";
+
+    public static final String START_GAME = "숫자야구 게임을 시작합니다.\n숫자 3자리를 입력해주세요.";
+
+}
