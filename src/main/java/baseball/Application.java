92c147e041834f2e396d59d4d049980fbf311fb3
KimTH94 <kcc940602@gmail.com>
2022-04-15 15:30:27 +0900

    feat(Application) add print method

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,16 @@
 package baseball;
 
+import baseball.common.SystemMessage;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+
+   public static void main(String[] args) {
+        print(SystemMessage.START_PROGRAM);
+        print(SystemMessage.END_PROGRAM);
+    }
+
+    private static void print(String message) {
+        System.out.println(message);
     }
 }
