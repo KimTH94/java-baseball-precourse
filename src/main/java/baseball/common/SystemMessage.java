7f96d1fe1c0ba270fbb67c87254af32668025eae
KimTH94 <kcc940602@gmail.com>
2022-04-17 16:15:38 +0900

    feat(SystemMessage) modify 유저 숫자 입력 문구

--- src/main/java/baseball/common/SystemMessage.java
+++ src/main/java/baseball/common/SystemMessage.java
@@ -5,7 +5,7 @@ public class SystemMessage {
     public static final String START_PROGRAM = "숫자 야구 프로그램을 시작합니다.";
     public static final String END_PROGRAM = "숫자 야구 프로그램을 종료합니다.";
 
-    public static final String START_GAME = "숫자야구 게임을 시작합니다.\n 1 ~ 9 사이 숫자 3자리를 입력해주세요.";
+    public static final String START_GAME = "숫자야구 게임을 시작합니다.\n1 ~ 9 사이 숫자 3자리를 입력해주세요.";
 
     public static final String NUMBER_EXCEPTION = "숫자만 입력해주세요.";
     public static final String DUPLICATE_EXCEPTION = "서로 다른 숫자를 입력해주세요.";
